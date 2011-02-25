ccca75b8fe01254f1eb465769ce34772e73592d7
eric <eric@guru.(none)>
2011-02-25 10:56:33 -0800

    changing the 1d histogram plotter

--- Plot1DHistograms.py
+++ Plot1DHistograms.py
@@ -77,12 +77,12 @@ yi = numpy.zeros(num)
 for c in cdfs:
 	yi = yi + c[1]
 
-yi = yi / yi.sum()
+yi = yi / yi.max()
 #histo,edges = numpy.histogram(xi, weights=yi, normed=True, bins=400)
 
 fig = plt.figure(num=1, facecolor='w', edgecolor='w', frameon=True)
 axs = fig.add_subplot(1,1,1)
-axs.plot(xi,yi,linestyle=':')
+axs.plot(xi,yi)
 	
 # cubic fitting
 '''
@@ -90,13 +90,14 @@ tck = interpolate.splrep(xi,yi,s=0)
 xnew = numpy.arange(min(xi),max(xi),0.01)
 ynew = interpolate.splev(xnew,tck,der=0)
 '''
-ynew = smooth(yi,window_len=25,window='hamming')
-axs.plot(xi,ynew,linewidth=4,color='k')
+#ynew = smooth(yi,window_len=25,window='hamming')
+#axs.plot(xi,ynew,linewidth=4,color='k')
 
 xticks(fontsize=48)
-plt.xlim(-1.0,1.0)
+#plt.xlim(-1.0,1.0)
 
 yticks([])
+
 
 name = cdfs[0].filename
 if "theta" in name or "oh" in name:
@@ -121,9 +122,9 @@ else:
 numpy.savetxt(sys.argv[1]+".1d.txt", (xi,ynew), fmt="%12.6e")
 '''
 
-import csv
-writer = csv.writer(open(sys.argv[1]+".1d.txt", "wb"),delimiter=' ')
-writer.writerows(zip(xi,ynew))
+#import csv
+#writer = csv.writer(open(sys.argv[1]+".1d.txt", "wb"),delimiter=' ')
+#writer.writerows(zip(xi,ynew))
 
 
 plt.show()
